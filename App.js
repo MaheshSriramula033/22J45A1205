267b857eb5c68627fd62755b3872c315d4fc0528
MaheshSriramula033 <162902604+MaheshSriramula033@users.noreply.github.com>
2024-08-17 12:33:24 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NumberFetcher from './NumberFetcher';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Average Calculator</h1>
+      <NumberFetcher />
+    </div>
+  );
+}
+
+export default App;
